6a278467422eabdfa2167fa445fb11afd764111b
user <user@A24COMP2>
2019-03-03 13:01:02 +0200

    WPF view models

--- TestSolution/TestProjectWPF/MainWindow.xaml.cs
+++ TestSolution/TestProjectWPF/MainWindow.xaml.cs
@@ -12,16 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestProjectWPF.Models;
 
 namespace TestProjectWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        UserViewModel user;
+
         public MainWindow()
         {
+            user = new UserViewModel();
             InitializeComponent();
         }
     }
---
+++ TestSolution/TestProjectWPF/Models/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectWPF.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public int Phone { get; set; }
+    }
+}
